437aa217bceaa14d0e627a2a3ff825874d883bfd
FireTheLost <prajwalmsreddy@gmail.com>
2022-01-04 11:50:25 +0530

    Create First Models

--- FireMedia2/urls.py
+++ FireMedia2/urls.py
@@ -15,7 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from django.views.generic import RedirectView
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
+    # Temporary Redirect From The Home Page To The Blog Page
+    path('', RedirectView.as_view(url='blog/', permanent=False)),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+import uuid
+
+
+class Blog(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='The Random ID For The Blog Post')
+
+    title = models.CharField(max_length=256)
+    description = models.CharField(max_length=64)
+    author = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
+
+    body = models.TextField(max_length=65536, help_text='The Main Body Of The Blog Post')
+    published = models.DateField(null=False, blank=False)
+
+    visibility = models.CharField(max_length=16)
+
+    def __str__(self):
+        return f'{self.title}\n{self.description} - {self.author}\n{self.published}'
+
+    def get_absolute_url(self):
+        return reverse('blog-post', args=[str(self.id)])
+
+
+class User(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='The Random ID For The User')
+
+    name = models.CharField(max_length=128)
+    email = models.CharField(max_length=128)
+    username = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f'{self.username}'
